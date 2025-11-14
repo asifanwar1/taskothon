ef9563100f6f8a2112e9e9024ea629ad2059f321
asifanwar1 <asifanwar0705@gmail.com>
2025-11-14 18:49:31 +0500

    fixed manifest file

--- components/ServiceWorker/ServiceWorker.tsx
+++ components/ServiceWorker/ServiceWorker.tsx
@@ -1,31 +1,3 @@
-// "use client";
-
-// import { useEffect } from "react";
-
-// export const ServiceWorker = (): null => {
-//     useEffect(() => {
-//         if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-//             navigator.serviceWorker
-//                 .register("/sw.js", {
-//                     scope: "/",
-//                     updateViaCache: "none",
-//                 })
-//                 .then((registration) => {
-//                     console.log("Service Worker registered:", registration);
-
-//                     registration.addEventListener("updatefound", () => {
-//                         console.log("Service Worker update found");
-//                     });
-//                 })
-//                 .catch((error) => {
-//                     console.error("Service Worker registration failed:", error);
-//                 });
-//         }
-//     }, []);
-
-//     return null;
-// };
-
 "use client";
 
 import { useEffect } from "react";
@@ -33,13 +5,11 @@ import { useEffect } from "react";
 export const ServiceWorker = (): null => {
     useEffect(() => {
         if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-            // Unregister old service workers first
             navigator.serviceWorker
                 .getRegistrations()
                 .then((registrations) => {
                     return Promise.all(
                         registrations.map((registration) => {
-                            // Unregister old versions
                             if (
                                 registration.active?.scriptURL.includes("sw.js")
                             ) {
@@ -50,7 +20,6 @@ export const ServiceWorker = (): null => {
                     );
                 })
                 .then(() => {
-                    // Register new service worker
                     return navigator.serviceWorker.register("/sw.js", {
                         scope: "/",
                         updateViaCache: "none",
@@ -59,10 +28,8 @@ export const ServiceWorker = (): null => {
                 .then((registration) => {
                     console.log("Service Worker registered:", registration);
 
-                    // Force update check
                     registration.update();
 
-                    // Check for updates
                     registration.addEventListener("updatefound", () => {
                         console.log("Service Worker update found");
                         const newWorker = registration.installing;
@@ -72,7 +39,6 @@ export const ServiceWorker = (): null => {
                                     newWorker.state === "installed" &&
                                     navigator.serviceWorker.controller
                                 ) {
-                                    // New service worker available, reload to activate
                                     console.log(
                                         "New service worker installed, reloading..."
                                     );
--- public/sw.js
+++ public/sw.js
@@ -1,108 +1,3 @@
-// const CACHE_NAME = "taskothon-v1";
-// const urlsToCache = [
-//     "/",
-//     "/tasks",
-//     "/auth/signin",
-//     "/icon-192x192.png",
-//     "/icon-512x512.png",
-// ];
-
-// const shouldCache = (url) => {
-//     if (
-//         url.includes("/_next/") ||
-//         url.includes("/api/") ||
-//         url.includes("/sw.js") ||
-//         url.includes("/manifest.json") ||
-//         url.includes("webpack") ||
-//         url.includes("chunks")
-//     ) {
-//         return false;
-//     }
-//     return true;
-// };
-
-// self.addEventListener("install", (event) => {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME).then((cache) => {
-//             return cache.addAll(urlsToCache);
-//         })
-//     );
-// });
-
-// self.addEventListener("activate", (event) => {
-//     event.waitUntil(
-//         caches.keys().then((cacheNames) => {
-//             return Promise.all(
-//                 cacheNames.map((cacheName) => {
-//                     if (cacheName !== CACHE_NAME) {
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
-
-// self.addEventListener("fetch", (event) => {
-//     const url = new URL(event.request.url);
-//     const requestUrl = event.request.url;
-
-//     if (
-//         !shouldCache(url.pathname) ||
-//         requestUrl.includes("/_next/") ||
-//         requestUrl.includes("webpack") ||
-//         requestUrl.includes("chunks") ||
-//         requestUrl.includes("/manifest.json")
-//     ) {
-//         event.respondWith(fetch(event.request));
-//         return;
-//     }
-
-//     event.respondWith(
-//         caches.match(event.request).then((response) => {
-//             if (response) {
-//                 return response;
-//             }
-
-//             return fetch(event.request).then((response) => {
-//                 if (response.status === 200 && shouldCache(url.pathname)) {
-//                     const responseToCache = response.clone();
-//                     caches.open(CACHE_NAME).then((cache) => {
-//                         cache.put(event.request, responseToCache);
-//                     });
-//                 }
-//                 return response;
-//             });
-//         })
-//     );
-// });
-// self.addEventListener("push", (event) => {
-//     if (event.data) {
-//         const data = event.data.json();
-//         const options = {
-//             body: data.body,
-//             icon: data.icon || "/icon-192x192.png",
-//             badge: "/icon-192x192.png",
-//             vibrate: [100, 50, 100],
-//             data: {
-//                 dateOfArrival: Date.now(),
-//                 primaryKey: "1",
-//             },
-//         };
-//         event.waitUntil(
-//             self.registration.showNotification(
-//                 data.title || "Taskothon",
-//                 options
-//             )
-//         );
-//     }
-// });
-
-// self.addEventListener("notificationclick", (event) => {
-//     event.notification.close();
-//     event.waitUntil(clients.openWindow("/"));
-// });
-
 const CACHE_NAME = "taskothon-v2";
 const urlsToCache = [
     "/",
@@ -112,23 +7,25 @@ const urlsToCache = [
     "/icon-512x512.png",
 ];
 
+const EXCLUDE_FROM_CACHE = [
+    "/_next/",
+    "/api/",
+    "/sw.js",
+    "/manifest.json",
+    "webpack",
+    "chunks",
+];
+
+const EXCLUDE_FROM_INTERCEPT = [
+    "dexie.cloud",
+    "/_next/",
+    "webpack",
+    "chunks",
+    "/manifest.json",
+];
+
 const shouldCache = (url) => {
-    if (url.includes("/_next/")) {
-        return false;
-    }
-    if (url.includes("/api/")) {
-        return false;
-    }
-    if (url.includes("/sw.js")) {
-        return false;
-    }
-    if (url.includes("/manifest.json")) {
-        return false;
-    }
-    if (url.includes("webpack") || url.includes("chunks")) {
-        return false;
-    }
-    return true;
+    return !EXCLUDE_FROM_CACHE.some((pattern) => url.includes(pattern));
 };
 
 const isDexieCloudRequest = (url) => {
@@ -136,22 +33,9 @@ const isDexieCloudRequest = (url) => {
 };
 
 const shouldIntercept = (requestUrl) => {
-    if (isDexieCloudRequest(requestUrl)) {
-        return false;
-    }
-    if (requestUrl.includes("/_next/")) {
-        return false;
-    }
-    if (requestUrl.includes("webpack")) {
-        return false;
-    }
-    if (requestUrl.includes("chunks")) {
-        return false;
-    }
-    if (requestUrl.includes("/manifest.json")) {
-        return false;
-    }
-    return true;
+    return !EXCLUDE_FROM_INTERCEPT.some((pattern) =>
+        requestUrl.includes(pattern)
+    );
 };
 
 self.addEventListener("install", (event) => {
