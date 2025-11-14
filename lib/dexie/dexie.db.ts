5c88916bd2a2f849a03e798c95ebd1420bdcb131
asifanwar1 <asifanwar0705@gmail.com>
2025-11-14 16:25:15 +0500

    change cloud url

--- lib/dexie/dexie.db.ts
+++ lib/dexie/dexie.db.ts
@@ -12,7 +12,7 @@ export class TaskDB extends Dexie {
             tasks: "@id, title, description, date, time, jiraLink, status, category",
         });
 
-        const databaseUrl = process.env.NEXT_PUBLIC_DEXIE_URL;
+        const databaseUrl = "https://zz8g27g95.dexie.cloud";
 
         if (!databaseUrl) {
             throw new Error(
